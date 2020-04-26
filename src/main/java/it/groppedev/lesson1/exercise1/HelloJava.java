fa91eb877bf08a89c882efe5ba83a3eae3afd208
groppedev <massimo.groppelli@gmail.com>
2020-04-26 15:26:27 +0200

    first commit

---
+++ src/main/java/it/groppedev/lesson1/exercise1/HelloJava.java
@@ -0,0 +1,12 @@
+package it.groppedev.lesson1.exercise1;
+
+/**
+ * @author groppedev
+ */
+public class HelloJava
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Hello Java!");
+	}
+}
---
+++ src/main/java/it/groppedev/lesson1/exercise1/README.md
@@ -0,0 +1,6 @@
+# Lesson 1 - Exercise 1
+## Versione Italiana
+
+## English Version
+
+
