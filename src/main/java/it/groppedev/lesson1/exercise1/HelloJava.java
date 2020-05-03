07f30deaffb264745947c937f3e2f7ac70ee94d3
groppedev <massimo.groppelli@gmail.com>
2020-05-03 23:47:53 +0200

    Improved HelloJava & added navigation images

--- src/main/java/it/groppedev/lesson1/exercise1/HelloJava.java
+++ src/main/java/it/groppedev/lesson1/exercise1/HelloJava.java
@@ -1,12 +1,18 @@
 package it.groppedev.lesson1.exercise1;
 
 /**
+ * @see https://dzone.com/articles/java-string-format-examples
  * @author groppedev
  */
 public class HelloJava
 {
+	private static final int JAVA_YEAR_OF_BIRTH = 1995;
+	
 	public static void main(String[] args)
 	{
-		System.out.println("Hello Java!");
+		int currentYear = java.time.Year.now().getValue();
+		int javaAge = currentYear - JAVA_YEAR_OF_BIRTH;
+		
+		System.out.println(String.format("Ciao, sono JAVA ed ho %d anni!", javaAge));
 	}
 }
